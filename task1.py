f853fe55db02aa19d453b9e02685f0c2d4fef4f0
osad-dm <frozenseller@gmail.com>
2018-12-04 01:57:52 +0600

    Add plots

--- task1.py
+++ task1.py
@@ -51,8 +51,6 @@ def psi_init(psi_vector, fock_dim):
     psi_list = []
     for i in range(0,len(psi_vector)):
         psi_list.append(fock(fock_dim,psi_vector[i]))
-    print("LOOOOOOOOOL")
-    print(psi_list)
     return tensor(psi_list)
 
 def uniform_array(num_wg, b, k):
@@ -65,59 +63,68 @@ def uniform_array(num_wg, b, k):
                 ham_matrix[i][j] = k
     return ham_matrix
 
-n = 6
+
+def fock_trans(q, n, m):
+    num = 0
+    state_list = []
+    for i in q:
+        if i != 0:
+            state_list.append((num, i))
+        num = num+1
+    base = n
+    newNum_list = []
+    j = 0
+    for i in state_list:
+        newNum_list.append(['', i[1]])
+        basisState = i[0]
+        while basisState > 0 or len(newNum_list[j][0]) < m:
+            newNum_list[j][0] = str(basisState % base) + newNum_list[j][0]
+            basisState //= base
+        j = j + 1
+
+    return newNum_list
+
+n = 4
 psi_vector = np.zeros(n)
 psi_vector = psi_vector.astype(int)
 psi_vector[2] = 1
-psi_vector[3] = 1
+psi_vector[1] = 1
+
 
 psi0 = psi_init(psi_vector, 3)
-print("________PSI")
-#print(psi0)
-print("________PSI0")
-
-hm = uniform_array(n, 1, 1)
-print(hm)
-print("________HM")
+hm = uniform_array(n, 1, np.sqrt(2)/2)
 qws = system(hm, psi0, 3)
-print("________ASTART")
-#print(qws.a)
-print("________A")
-times = np.linspace(0,5,100)
+times = np.linspace(0,1, 2)
 
 start = time.time()
-#result_un = sesolve(qws.H, psi0, times, [])
-end = time.time()
-print(end - start)
+result_un = sesolve(qws.H, psi0, times, [])
 
-#result_loss = mesolve(qws.H, psi0, times, qws.gen_c_ops(0.25), [])
-
-
-'''n = list(range(2,11))
-calc_time = []
-mem_use = []
-mem_bg = 0
-process = psutil.Process(os.getpid())
-for i in range(0,len(n)):
-    psi_vector = np.zeros(n[i])
-    psi_vector = psi_vector.astype(int)
-    psi_vector[int(round(n[i]/2))] = 1
-    psi_vector[int(round(n[i]/2-1))] = 1
-    psi0 = psi_init(psi_vector, 3)
-    hm = uniform_array(n[i], 1, 1)
-    qws = system(hm, psi0, 3)
-    times = np.linspace(0,5,100)
-    start = time.time()
-    result_un = sesolve(qws.H, psi0, times, [])
-    end = time.time()
-    calc_time.append(end - start)
-    mem_use.append(process.memory_info().rss/1024/1024)
-    print(process.memory_info().rss/1024/1024)
-    print(end - start)
-    
-plt.semilogy(n, calc_time, label = 'Two-photons')
-plt.legend()
-plt.ylabel('Calculation time (sec)')
-plt.xlabel('Number of modes')
+amps=[]
+ticks=[]
+HEH = fock_trans(result_un.states[1], 3, 4)
+print(HEH)
+sum = 0
+for i in HEH:
+    amps.append(abs(i[1][0][0])**2)
+    ticks.append(i[0])
+for i in amps:
+    sum = sum + i
+print(sum)
+fig, ax = plt.subplots()
+plt.bar(np.arange(1,len(amps)+1), amps)
+plt.xticks(np.arange(1,len(amps)+1), ticks)
+plt.ylim(0,1)
+plt.tight_layout()
 plt.show()
-'''
+
+''' fig, axes = plt.subplots(1, len(result_un.states))
+for i in range(0, len(result_un.states)):
+    print(i," vector")
+    print(result_un.states[i])
+    print("Fock", i)
+    print(fock_trans(result_un.states[i], 2, 4))
+    plot_fock_distribution(result_un.states[i], fig=fig, ax=axes[i], title="Fock state");
+
+plt.show()'''
+
+
